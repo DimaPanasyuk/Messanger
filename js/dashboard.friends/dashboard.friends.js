743cf3aec1680a4b69fc5b207079bdd4b65209b9
Dima Panasyuk <dima_panasyuk@ukr.net>
2016-05-23 13:22:57 +0300

    added functionality for adding users to friends list

--- js/dashboard.friends/dashboard.friends.js
+++ js/dashboard.friends/dashboard.friends.js
@@ -1,5 +1,56 @@
 export default
-['$scope', '$rootScope', function($scope, $rootScope) {
+['$scope', '$rootScope', 'userInfo', '$timeout', 
+ 'fire', '$firebaseObject', '$firebaseArray',
+function($scope, $rootScope, userInfo, $timeout, 
+ fire, $firebaseObject, $firebaseArray) {
   
+  $rootScope.loading = true;
   $scope.pageTitle = 'List of your friends';
+  let friends_ref  = new Firebase(`${fire}/users/${userInfo.uid}/friends`),
+      friends      = $firebaseArray(friends_ref),
+      current_user_friends_ref = new Firebase(`${fire}/users/${userInfo.uid}/friends`),
+      current_user_friends     = $firebaseArray(current_user_friends_ref);
+  
+  friends.$loaded(function() {
+    
+    console.debug(friends);
+    $scope.friends = friends;
+    $rootScope.loading = false;
+  })
+  
+  $scope.filter        = 'show-all';
+  $scope.removeFriend  = removeFriend;
+  $scope.filterFriends = filterFriends;
+  
+  function filterFriends(state) {
+    
+    $scope.filter = state;
+    switch(state) {
+      
+      case 'show-all':
+        $scope.friends = friends;
+        break;
+      case 'show-online':
+        $scope.friends = friends
+        .filter(function(friend) {
+          
+          return friend.lastLoggedOut === 0;
+        });
+    }
+  }
+  
+  
+  function removeFriend(friend, index) {
+    
+    let friend_ref = new Firebase(`${fire}/users/${userInfo.uid}/friends/${friend.$id}`),
+        friend_u   = $firebaseObject(friend_ref);      
+    friend_u
+    .$remove()
+    .then(function(data) {
+      
+      _.remove($scope.friends, {
+        $id: friend.$id
+      })
+    })
+  }
 }]
--- js/dashboard.users/dashboard.users.js
+++ js/dashboard.users/dashboard.users.js
@@ -1,13 +1,19 @@
+import _ from 'lodash';
+
 export default
 ['$scope', '$rootScope', 'userInfo',
- 'fire', '$firebaseArray',
+ 'fire', '$firebaseArray', '$firebaseObject',
 
-function($scope, $rootScope, userInfo, fire, $firebaseArray) {
+function($scope, $rootScope, userInfo, fire, $firebaseArray, $firebaseObject) {
   
-  $rootScope.loading = true;
-  let users_ref = new Firebase(`${fire}/users`),
-      users     = $firebaseArray(users_ref);
-
+  $rootScope.loading           = true;
+  let users_ref                = new Firebase(`${fire}/users`),
+      users                    = $firebaseArray(users_ref),
+      current_user_friends_ref = new Firebase(`${fire}/users/${userInfo.uid}/friends`),
+      current_user_friends     = $firebaseArray(current_user_friends_ref);
+  
+  $scope.current_user_friends = current_user_friends;
+  
   users.$loaded(function() {
     
     $rootScope.loading = false;
@@ -19,12 +25,29 @@ function($scope, $rootScope, userInfo, fire, $firebaseArray) {
     .filter(function(user) {
       
       return user.id !== userInfo.uid;
-    });
+    })
+    .map(function(user) {
+      
+      if (_.find(current_user_friends, {
+        id: user.id
+      })) {
+        
+        user.$$friend = true;
+        return user;
+      } else {
+        
+        user.$$friend = false;
+        return user;
+      }
+    })
+    
+    console.debug($scope.users);
   });    
   
   $scope.pageTitle = 'Users page';
   $scope.filter = 'show-all';
   $scope.filterUsers = filterUsers;
+  $scope.toggleFriends = toggleFriends;
   
   
   
@@ -41,7 +64,21 @@ function($scope, $rootScope, userInfo, fire, $firebaseArray) {
         .filter(function(user) {
           
           return user.id !== userInfo.uid;
-        });
+        })
+        .map(function(user) {
+      
+          if (_.find(current_user_friends, {
+            id: user.id
+          })) {
+            
+            user.$$friend = true;
+            return user;
+          } else {
+            
+            user.$$friend = false;
+            return user;
+          }
+        })
         break;
       case 'show-online':
         $scope.users = users
@@ -57,6 +94,49 @@ function($scope, $rootScope, userInfo, fire, $firebaseArray) {
           
           return user.lastLoggedOut === 0;
         })
+        .map(function(user) {
+      
+          if (_.find(current_user_friends, {
+            id: user.id
+          })) {
+            
+            user.$$friend = true;
+            return user;
+          } else {
+            
+            user.$$friend = false;
+            return user;
+          }
+        })
+    }
+  }
+  
+  function toggleFriends(user, index) {
+    
+    if(_.find(current_user_friends, {
+      id: user.id 
+    })) {
+      
+      console.debug(user);
+      let user_ref = new Firebase(`${fire}/users/${userInfo.uid}/friends/${user.$id}`),
+          user_u   = $firebaseObject(user_ref); 
+         
+       user_u  
+        .$remove()
+        .then(function(data) {
+          console.debug('remove');
+          user.$$friend = false;
+        })
+    } else {
+      
+      current_user_friends_ref.child(user.id)
+        .set({
+          id: user.id,
+          info: user.info,
+          lastLoggedOut: user.lastLoggedOut,
+          lastLoggedIn: user.lastLoggedIn          
+        });
+        user.$$friend = true;
     }
   }
 }]
